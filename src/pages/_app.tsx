21a887631e9a769066e5a3d5f57ab9305d61a40b
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-09 18:56:30 -0300

    add fonts and title to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import { Roboto } from "next/font/google";
+import Head from "next/head";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Head>
+        <link rel="icon" href="/logo.svg" />
+        <title>Ignite Shop</title>
+        <meta property="og:title" content="Ignite Shop" key="title" />
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
 }
