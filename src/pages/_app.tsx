2bc7fe59097df49d7ba31fcc4ce843017154ffb3
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-10 15:49:57 -0300

    created base structure for Home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <Header>
-        <Image src={logoImg} alt="" />
+        <Image src={logoImg} alt=""/>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-import { styled } from "../styles";
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import shirt1 from "../assets/shirts/1.png"
+import shirt2 from "../assets/shirts/2.png"
+import shirt3 from "../assets/shirts/3.png"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 59,99</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 59,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
