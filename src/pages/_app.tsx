b901135c6d7eab91cbf7690abf303b4b10a723f5
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-10 14:09:16 -0300

    created global app header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,11 @@ import type { AppProps } from "next/app";
 
 import { Roboto } from "next/font/google";
 import Head from "next/head";
+import Image from "next/image";
 
 import { globalStyles } from "../styles/global";
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -15,13 +18,17 @@ globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <Container className={roboto.className}>
       <Head>
         <link rel="icon" href="/logo.svg" />
         <title>Ignite Shop</title>
         <meta property="og:title" content="Ignite Shop" key="title" />
       </Head>
+
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
       <Component {...pageProps} />
-    </main>
+    </Container>
   );
 }
