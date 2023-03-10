99dce5b487f45a34b95807cfd662dd21f908bf82
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-10 16:41:34 -0300

    crated carousel using keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+
 import { HomeContainer, Product } from "../styles/pages/home";
 
-import shirt1 from "../assets/shirts/1.png"
-import shirt2 from "../assets/shirts/2.png"
-import shirt3 from "../assets/shirts/3.png"
+import shirt1 from "../assets/shirts/1.png";
+import shirt2 from "../assets/shirts/2.png";
+import shirt3 from "../assets/shirts/3.png";
+
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt="" />
 
         <footer>
@@ -17,11 +28,29 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta 2</strong>
+          <span>R$ 59,99</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 59,99</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 3</strong>
           <span>R$ 59,99</span>
         </footer>
       </Product>
