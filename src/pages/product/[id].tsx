77119ecf18de2a3423820cd7846c6d60deff4c2c
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-11 14:41:08 -0300

    created products page visual structure and used Next Link to navigate through the pages

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,32 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
 
 export default function Product() {
-  const { query } = useRouter()
+  const { query } = useRouter();
 
- return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          numquam nam at corrupti porro molestias ratione vero eos, maxime, ut
+          obcaecati delectus expedita velit pariatur illum! Consequuntur atque
+          deserunt minus!
+        </p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
