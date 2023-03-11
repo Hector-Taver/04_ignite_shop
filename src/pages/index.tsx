2791b9103b20e6f32b40c72950448d199f36c1df
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-11 20:22:04 -0300

    created visual structure of the success page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { GetStaticProps } from "next";
 import { useKeenSlider } from "keen-slider/react";
 import Stripe from "stripe";
@@ -29,25 +30,26 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product
+          <Link
             href={`/product/${product.id}`}
             key={product.id}
-            className="keen-slider__slide"
             prefetch={false}
           >
-            <Image
-              src={product.imageUrl}
-              width={520}
-              height={480}
-              alt=""
-              priority
-            />
+            <Product className="keen-slider__slide">
+              <Image
+                src={product.imageUrl}
+                width={520}
+                height={480}
+                alt=""
+                priority
+              />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         );
       })}
     </HomeContainer>
